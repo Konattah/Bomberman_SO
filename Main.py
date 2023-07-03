29003b4ee097065ea8ec1b2ba7d585baa7fcb682
Konattah <carlos.s.carrasco.z@gmail.com>
2023-07-02 22:41:28 -0400

    Menu

--- Main.py
+++ Main.py
@@ -5,91 +5,93 @@ import random
 from Escenario import irrompible, rompible
 from Jugador import jugador
 
-# Constantes
-bloques_tamanio = 40
+def main():
+    # Constantes
+    bloques_tamanio = 40
 
 
-suelo = [[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
-         [-1, 3, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
-         [-1, 0, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
-         [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
-         [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
-         [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
-         [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
-         [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
-         [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
-         [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
-         [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
-         [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
-         [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
-         [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 4, -1],
-         [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]]
+    suelo = [[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, 3, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 0, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1],
+            [-1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1, 1, -1],
+            [-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 4, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]]
 
-# Cosas de la ventana
-ancho_ventana = 760
-alto_ventana = 600
-ventana = pygame.display.set_mode((ancho_ventana, alto_ventana))
-pygame.display.set_caption("Bomberman")
+    # Cosas de la ventana
+    ancho_ventana = 760
+    alto_ventana = 600
+    ventana = pygame.display.set_mode((ancho_ventana, alto_ventana))
+    pygame.display.set_caption("Bomberman")
 
-#Reloj
-clock = pygame.time.Clock()
-fps = 30
-
-
-# Grupos de sprites
-all_rompibles = pygame.sprite.Group()
-all_irrompibles = pygame.sprite.Group()
-all_jugador = pygame.sprite.Group()
-all_bombas = pygame.sprite.Group()
-all_sprites = pygame.sprite.Group()
+    #Reloj
+    clock = pygame.time.Clock()
+    fps = 30
 
 
-game_over = False
+    # Grupos de sprites
+    all_rompibles = pygame.sprite.Group()
+    all_irrompibles = pygame.sprite.Group()
+    all_jugador = pygame.sprite.Group()
+    all_bombas = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
 
-for f in range(len(suelo)):
-    for c in range(len(suelo[f])):
-        if suelo[f][c] == -1:
-            all_irrompibles.add(irrompible(c, f, bloques_tamanio))
-        elif suelo[f][c] == 1:
-            azar = random.randint(1, 4)
-            if azar == 1 or azar == 3:
-                madera = rompible(c, f, bloques_tamanio)
-                all_rompibles.add(madera)
-                suelo[f][c] = 1
-            else:
+
+    game_over = False
+
+    for f in range(len(suelo)):
+        for c in range(len(suelo[f])):
+            if suelo[f][c] == -1:
+                all_irrompibles.add(irrompible(c, f, bloques_tamanio))
+            elif suelo[f][c] == 1:
+                azar = random.randint(1, 4)
+                if azar == 1 or azar == 3:
+                    madera = rompible(c, f, bloques_tamanio)
+                    all_rompibles.add(madera)
+                    suelo[f][c] = 1
+                else:
+                    suelo[f][c] = 0
+            elif suelo[f][c] == 3:
+                jugador1 = jugador(c, f, all_bombas, all_sprites, all_rompibles, bloques_tamanio, suelo, all_jugador)
+                all_jugador.add(jugador1)
                 suelo[f][c] = 0
-        elif suelo[f][c] == 3:
-            jugador1 = jugador(c, f, all_bombas, all_sprites, all_rompibles, bloques_tamanio, suelo, all_jugador)
-            all_jugador.add(jugador1)
-            suelo[f][c] = 0
 
 
-all_sprites.add(all_irrompibles)
-all_sprites.add(all_rompibles)
-all_sprites.add(all_jugador)
+    all_sprites.add(all_irrompibles)
+    all_sprites.add(all_rompibles)
+    all_sprites.add(all_jugador)
 
-while(game_over == False):
-    # Eventitos que van sucediendo
-    for evento in pygame.event.get():
-        # Evento por si apretan la X de la ventana
-        if evento.type == pygame.QUIT:
-            game_over = True
+    while(game_over == False):
+        # Eventitos que van sucediendo
+        for evento in pygame.event.get():
+            # Evento por si apretan la X de la ventana
+            if evento.type == pygame.QUIT:
+                game_over = True
 
-    ventana.fill((255, 192, 203))
+        ventana.fill((255, 192, 203))
 
-    # Se guarda la tecla que se vaya a presionar
-    tecla = pygame.key.get_pressed()
+        # Se guarda la tecla que se vaya a presionar
+        tecla = pygame.key.get_pressed()
 
-    jugador1.update(tecla)
-    all_bombas.update()
+        jugador1.update(tecla)
+        all_bombas.update()
 
-    # Dibujos
-    all_sprites.draw(ventana)
+        # Dibujos
+        all_sprites.draw(ventana)
 
-    # Se renderiza el juego desde acá
-    pygame.display.flip()
-    
-    # Limita los fps del juego
-    clock.tick(fps) 
+        # Se renderiza el juego desde acá
+        pygame.display.flip()
+        
+        # Limita los fps del juego
+        clock.tick(fps) 
 
-pygame.quit()
+    pygame.quit()
+
--- Menu.py
+++ Menu.py
@@ -1,3 +1,56 @@
 import pygame
+import os
+from Main import main
 
 pygame.init()
+
+# Cosas de la ventana
+ancho_ventana = 760
+alto_ventana = 600
+ventana = pygame.display.set_mode((ancho_ventana, alto_ventana))
+pygame.display.set_caption("Bomberman")
+
+#Reloj
+clock = pygame.time.Clock()
+fps = 30
+
+fondo_img = pygame.image.load(os.path.join('Imagenes', 'Boton', 'fondo.png')).convert_alpha()
+iniciar_img = pygame.image.load(os.path.join('Imagenes', 'Boton', 'iniciar.png')).convert_alpha()
+salir_img = pygame.image.load(os.path.join('Imagenes', 'Boton', 'salir.png')).convert_alpha()
+
+ancho_botones = iniciar_img.get_width()
+alto_botones = iniciar_img.get_height()
+
+# Definir las posiciones de los botones
+posicion_abrir = (((ancho_ventana - ancho_botones) // 2), ((alto_ventana - alto_botones) // 2))
+posicion_cerrar = (((ancho_ventana - ancho_botones) // 2), ((alto_ventana - alto_botones) // 2 + 180))
+
+
+
+game_over = False
+
+while(game_over == False):
+    # Eventitos que van sucediendo
+    for evento in pygame.event.get():
+        # Evento por si apretan la X de la ventana
+        if evento.type == pygame.QUIT:
+            game_over = True
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            if posicion_abrir[0] < evento.pos[0] < posicion_abrir[0] + ancho_botones and posicion_abrir[1] < evento.pos[1] < posicion_abrir[1] + alto_botones:
+                main()
+                game_over = True
+            
+            elif posicion_cerrar[0] < evento.pos[0] < posicion_cerrar[0] + ancho_botones and posicion_cerrar[1] < evento.pos[1] < posicion_cerrar[1] + alto_botones:
+                game_over = True
+
+    ventana.blit(fondo_img, (0, 0))
+    ventana.blit(iniciar_img, posicion_abrir)
+    ventana.blit(salir_img, posicion_cerrar)
+
+    # Se renderiza el juego desde acá
+    pygame.display.flip()
+    
+    # Limita los fps del juego
+    clock.tick(fps) 
+
+pygame.quit()
